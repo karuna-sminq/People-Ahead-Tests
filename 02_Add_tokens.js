35f2a686f33f5687ce8a1530f31bda84cddb9087
karuna <karunal@sminq.com>
2017-03-22 15:14:24 +0530

    Modified add bulk tokens script - removed test checks

--- 02_Add_tokens.js
+++ 02_Add_tokens.js
@@ -39,6 +39,7 @@ describe("Add Tokens", function() {
         for (var i = 0; i < token_list.length; i++) {
 
             // console.log(token_list[i]);
+
             //Token Details
             element(by.model('mobile')).sendKeys(token_list[i]);
 
@@ -47,64 +48,9 @@ describe("Add Tokens", function() {
             //Join
             element(by.css('[ng-click="join(mobile, name, reason, selectedQueue, appointmentDate, selectedSlot)"]')).click();
 
-            browser.sleep(5000);
-
-            expect(element(by.css('.md-toast-content')).getText()).toEqual('Token created successfully');
+            browser.sleep(sleep_time);
 
         }
-
-        //
-        // //Queue Selection
-        // element(by.model('selectedQueue')).click();
-        //
-        // browser.sleep(sleep_time);
-        //
-        // var doc_qsel = element.all(by.repeater('queue in queueList'));
-        //
-        // doc_qsel.get(1).click();
-        //
-        // browser.sleep(sleep_time);
-        //
-        // /*= End of 1st sminq =*/
-        // /*=============================================<<<<<*/
-        //
-        // /*=============================================>>>>>
-        // = 2nd Sminq =
-        // ===============================================>>>>>*/
-        //
-        // element(by.css('.add-new-button')).click();
-        //
-        // browser.sleep(sleep_time);
-        //
-        // //Token Details
-        // element(by.model('mobile')).sendKeys(8974398989);
-        //
-        // browser.sleep(sleep_time);
-        //
-        // element(by.model('reason')).sendKeys('fever');
-        //
-        // browser.sleep(sleep_time);
-        //
-        // //Queue Selection
-        // element(by.model('selectedQueue')).click();
-        //
-        // browser.sleep(sleep_time);
-        //
-        // var doc_qsel = element.all(by.repeater('queue in queueList'));
-        //
-        // doc_qsel.get(1).click();
-        //
-        // browser.sleep(sleep_time);
-        //
-        // //Join
-        // element(by.css('[ng-click="join(mobile, name, reason, selectedQueue, appointmentDate, selectedSlot)"]')).click();
-        //
-        // browser.sleep(5000);
-        //
-        // expect(element(by.css('.md-toast-content')).getText()).toEqual('Token created successfully');
-        //
-        // /*= End of 2nd Sminq =*/
-        // /*=============================================<<<<<*/
 
     });
 
